0d6516ed2f2c62646ef52c7de4f1024ef7e51e49
Malavika Suresh <malavikasuresh863@gmail.com>
2025-05-08 15:06:48 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,9 +10,9 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Adding the chatbot logic
 def chat_with_gpt(messages):
-  response = client.ChatCompletion.create(
+  response = client.chat.completions.create(
       model = "gpt-4",
       messages = messages,
       temperature = 0.7
   )
-  return response['choices'][0]['message']['content']
+  return response.choices[0].message.content
